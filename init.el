46507bcb0caa24eff96f165b5fdb46b25556e602
Peter Vlk <petervlk86@gmail.com>
2024-03-28 00:13:47 +0100

    create missing directories

--- init.el
+++ init.el
@@ -62,6 +62,16 @@
 ;; show line numbers
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
+;; Offer to create parent directories if they do not exist
+;; http://iqbalansari.github.io/blog/2014/12/07/automatically-create-parent-directories-on-visiting-a-new-file-in-emacs/
+(defun my-create-non-existent-directory ()
+  (let ((parent-directory (file-name-directory buffer-file-name)))
+    (when (and (not (file-exists-p parent-directory))
+               (y-or-n-p (format "Directory `%s' does not exist! Create it?" parent-directory)))
+      (make-directory parent-directory t))))
+
+(add-to-list 'find-file-not-found-functions 'my-create-non-existent-directory)
+
 (require 'crafted-completion-config)
 (global-corfu-mode 1)
 (custom-set-variables
